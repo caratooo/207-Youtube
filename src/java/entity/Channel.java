2b736afe2a4a2173ad2c5717a3ae0259f63bd5c3
cynthia <cyn.zhou@mail.utoronto.ca>
2023-10-01 16:11:12 -0400

    Channel class undated

--- src/java/entity/Channel.java
+++ src/java/entity/Channel.java
@@ -2,4 +2,16 @@ package entity;
 
 public class Channel {
     private String id;
+    private Integer viewcount;
+    private Integer subscriberCount;
+    private Boolean hiddenSubscriberCount;
+    private Integer videoCount;
+
+    public Channel(String id, Integer viewcount, Integer subscriberCount, Boolean hiddenSubscriberCount, Integer videoCount) {
+        this.id = id;
+        this.viewcount = viewcount;
+        this.subscriberCount = subscriberCount;
+        this.hiddenSubscriberCount = hiddenSubscriberCount;
+        this.videoCount = videoCount;
+    }
 }
